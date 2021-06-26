751aa35207a4c6a3cc911fcd4a62d7035c79d6bf
Andrew <andrescastillo.r@hotmail.com>
2021-06-25 22:53:43 -0400

    update placeOrder()

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
@@ -8,7 +8,34 @@ def index():
 
 @app.route("/ordenar", methods=['GET', 'POST'])
 def placeOrder():
-    return render_template('place_order.html')
+    if request.method == 'GET':
+        context = {}
+        # get data from the database to display
+		# context['sizes'] = json.dumps(list(Size.objects.all().values()))
+		# context['toppings'] = json.dumps(list(Topping.objects.all().values()))		
+        return render_template('place_order.html', context=context) # jsonify() is probably necessary here? I'll check later
+
+    if request.method == 'POST':
+        '''
+        order_info = json.loads(request.body) # obtains the info from the poll.
+		error, key = self.validateOrder(order_info)
+		if error:
+			return HttpResponseBadRequest(f"ERROR: the parameter {key} can't be empty.")
+		else:
+			request.session['_order'] = order_info # session var used in pizzeria:confirm_order url.
+			return HttpResponse('There are no empty fields.')
+        '''
+
+# this function which was written by the other collaborator is flawed and I'll probably delete it
+def validateOrder(self, order):
+    """Returns boolean that determines if an error ocurred and the key where the error ocurred"""
+    error = False
+    key = ''
+    for k, v in order.items(): # Checks each value of the dictionary, if the value is empty returns an 400 error
+        if v == '':
+            error = True
+            key = k
+    return error, key
 
 @app.route("/ordenar/confirmar")
 def confirmOrder():
