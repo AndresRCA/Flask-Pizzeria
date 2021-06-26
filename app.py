4e424d91f88babe0c91adb909005abeea3f3991f
Andrew <andrescastillo.r@hotmail.com>
2021-06-25 21:45:51 -0400

    set routes (bare bones)

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<p>Hello</p>"
+    return render_template('index.html')
+
+@app.route("/ordenar", methods=['GET', 'POST'])
+def placeOrder():
+    return render_template('place_order.html')
+
+@app.route("/ordenar/confirmar")
+def confirmOrder():
+    return render_template('confirm_order.html')
+
+@app.route("/ordenar/finalizar")
+def finalizeOrder():
+    return render_template('finalize_order.html')
