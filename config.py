f4dc8220d69867c86140417f92ecd7707c414147
Andrew <andrescastillo.r@hotmail.com>
2021-07-12 18:32:10 -0400

    add extra configuration to template

--- config.py
+++ config.py
@@ -7,3 +7,6 @@ class Config(object):
     FLASK_APP = os.environ.get('FLASK_APP')
     FLASK_ENV = os.environ.get('FLASK_ENV')
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+
+    # special environment vars are saved here as additional keys (watch out for name collision)
+    ENV_MODE = os.environ.get('ENV_MODE')
