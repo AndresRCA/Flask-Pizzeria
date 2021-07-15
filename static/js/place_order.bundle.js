99b96823c2e6c475827c76958750be4d5b3edf80
Andrew <andrescastillo.r@hotmail.com>
2021-07-14 20:26:20 -0400

    update .gitignore

--- static/js/place_order.bundle.js
+++
@@ -1,7 +0,0 @@
-/* 
-Your built code should go here, think of this as the build or dist folder (/static/js),
-although to be honest I'd prefer to have a separate directory named like that and not put the
-built code here inside static, but I'm just keeping it here because I don't want to use an
-absolute path inside the template and because I don't want a whole new folder for just a
-single file ¯\_(ツ)_/¯
-*/
