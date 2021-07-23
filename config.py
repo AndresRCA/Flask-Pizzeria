9db3001c5358efd07bd6ea9663875fe276dd09be
Andrew <andrescastillo.r@hotmail.com>
2021-07-22 21:21:24 -0400

    update config.py

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ class Config(object):
     FLASK_APP = os.environ.get('FLASK_APP')
     FLASK_ENV = os.environ.get('FLASK_ENV')
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # special environment vars are saved here as additional keys (watch out for name collision)
     ENV_MODE = os.environ.get('ENV_MODE')
